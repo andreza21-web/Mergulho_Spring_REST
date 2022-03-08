18229835b0dd5bc66211ae2ade907d4bb37f3283
andreza <andreza.21pessoa@gmail.com>
2022-03-08 19:57:41 -0300

    first commit

---
+++ src/main/java/com/dev/algaWorks/AlgaWorksApplication.java
@@ -0,0 +1,13 @@
+package com.dev.algaWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/algaWorks/AlgaWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.algaWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
